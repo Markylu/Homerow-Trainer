5117a081b4cc15e470f2153bdd357998bf1ea620
Markylu <46211683+Markylu@users.noreply.github.com>
2025-01-30 09:07:01 -0500

    Create livestreammodel.py

---
+++ trainer/livestreammodel.py
@@ -0,0 +1,83 @@
+# STEP 1: Import the necessary modules
+import mediapipe as mp
+import cv2
+import numpy as np
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import os
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()
+
+MARGIN = 30  # pixels
+FONT_SIZE = 5
+FONT_THICKNESS = 5
+HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
+modelpath = os.getenv("MODEL_PATH")
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+    hand_landmarks_list = detection_result.hand_landmarks
+    handedness_list = detection_result.handedness
+    annotated_image = np.copy(rgb_image)
+
+    # Convert RGB image to BGR
+    annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR)
+
+    # Loop through the detected hands to visualize.
+    for idx in range(len(hand_landmarks_list)):
+        hand_landmarks = hand_landmarks_list[idx]
+        handedness = handedness_list[idx]
+
+        # Draw the hand landmarks.
+        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        hand_landmarks_proto.landmark.extend([
+            landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+        ])
+        solutions.drawing_utils.draw_landmarks(
+            annotated_image,
+            hand_landmarks_proto,
+            solutions.hands.HAND_CONNECTIONS
+        )
+
+        # Draw handedness (left or right hand).
+        text_x = int(hand_landmarks[0].x * annotated_image.shape[1])
+        text_y = int(hand_landmarks[0].y * annotated_image.shape[0]) - MARGIN
+        cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                    (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                    FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+
+    return annotated_image
+
+
+def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('hand landmarker result: {}'.format(result))
+
+# STEP 2: Create an HandLandmarker object.
+base_options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path=modelpath),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+options = vision.HandLandmarkerOptions(base_options=base_options,
+                                       num_hands=2)
+
+with HandLandmarker.create_from_options(options) as landmarker:
+
+# STEP 3: Load the input image.
+image = mp.Image.create_from_file(imagepath)
+
+# STEP 4: Detect hand landmarks from the input image.
+detection_result = detector.detect(image)
+
+# STEP 5: Process the classification result. In this case, visualize it.
+
+annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+cv2.imshow("Annotated Image", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
